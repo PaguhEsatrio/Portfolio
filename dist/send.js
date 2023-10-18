d4c4e0850ec9e54d1d96e6900b7307c65a2e7aa1
PaguhEsatrio <paguh2015@gmail.com>
2023-10-18 20:47:49 +0700

    fixed

--- dist/send.js
+++ dist/send.js
@@ -1,51 +1,59 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbx4d6d3rQfNL9XiVQ00HXHvTpXr-9sVPzs8iWdivRCDnUxP5P-lD8NCbNqNSYpdDAMf/exec';
-    const form = document.forms['portfolio'];
-    const myAlert = document.querySelector('.alert-ku');
+const form = document.forms['portfolio'];
+const myAlert = document.querySelector('.alert-ku');
 
-    form.addEventListener('submit', e => {
-      e.preventDefault();
-      if (validateForm()) {
-        startLoading();
-        fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-          .then(response => {
-            form.reset();
-            myAlert.classList.toggle('opacity-0');
-            console.log('Success!', response)
-          })
-          .catch(error => console.error('Error!', error.message));
-      }
-    });
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  if (validateForm()) {
+    startLoading();
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+      .then(response => {
+        form.reset();
+        myAlert.classList.toggle('hidden');
+        console.log('Success!', response)
+      })
+      .catch(error => console.error('Error!', error.message));
+  }
+});
 
-    function validateForm() {
-      const nama = document.forms['portfolio']['nama'].value;
-      const email = document.forms['portfolio']['email'].value;
-      const pesan = document.forms['portfolio']['pesan'].value;
+const alertSalah = document.querySelector('.alert-salah');
 
-      if (nama === '' || email === '' || pesan === '') {
-        alert('Harap lengkapi semua field sebelum mengirim.');
-        return false;
-      }
-      return true;
-    }
+function validateForm() {
+  const nama = document.forms['portfolio']['nama'].value;
+  const email = document.forms['portfolio']['email'].value;
+  const pesan = document.forms['portfolio']['pesan'].value;
 
-    function startLoading() {
-      var loadingIcon = document.getElementById('loadingIcon');
-      var buttonText = document.getElementById('buttonText');
+  if (nama === '' || email === '' || pesan === '') {
+    alertSalah.classList.toggle('hidden');
+    return false;
+  }
+  return true;
+}
 
-      loadingIcon.style.display = 'inline-block';
-      buttonText.innerText = 'Loading...';
+function startLoading() {
+  var loadingIcon = document.getElementById('loadingIcon');
+  var buttonText = document.getElementById('buttonText');
 
-      // Simulate loading for 2 seconds
-      setTimeout(function () {
-        // Reset the button to initial state
-        loadingIcon.style.display = 'none';
-        buttonText.innerText = 'Kirim';
-      }, 1500);
-    }
+  loadingIcon.style.display = 'inline-block';
+  buttonText.innerText = 'Loading...';
 
-    const closeButton = document.getElementById('close-button');
-    const alertBox = document.getElementById('alert-border-1');
-  
-    closeButton.addEventListener('click', function () {
-      alertBox.classList.add('opacity-0');
-    });
+  // Simulate loading for 2 seconds
+  setTimeout(function () {
+    // Reset the button to initial state
+    loadingIcon.style.display = 'none';
+    buttonText.innerText = 'Kirim';
+  }, 2000);
+}
+
+const closeButton = document.getElementById('close-button');
+const closeButton1 = document.getElementById('close-button1');
+const alertBox = document.getElementById('alert-ku');
+const closeAlertSalah = document.getElementById('alert-salah');
+
+closeButton.addEventListener('click', function () {
+  alertBox.classList.add('hidden');
+});
+
+closeButton1.addEventListener('click', function () {
+  closeAlertSalah.classList.add('hidden');
+});
